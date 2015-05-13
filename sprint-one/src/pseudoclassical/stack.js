2164640aa9f1646fbdd7c5c2ed0b1a24c64c4b46
SYU88 <sarah.m.yu88@gmail.com>
2015-05-13 16:12:04 -0700

    Pseudoclassical stack: .push and .pop increment and decrement stackSize

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -2,7 +2,16 @@ var Stack = function() {
   this.stackSize = 0;
 };
 
-// Stack.prototype.push = function
 Stack.prototype.size = function(){
   return this.stackSize;
+};
+
+Stack.prototype.push = function(value){
+  this.stackSize++;
+};
+
+Stack.prototype.pop = function(){
+  if(this.stackSize > 0){
+    this.stackSize--;
+  }
 };
