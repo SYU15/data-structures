fab2d17daddc2d81c418c82421401473f0935fc1
SYU88 <sarah.m.yu88@gmail.com>
2015-05-13 16:24:26 -0700

    Pseudoclassical stack: .push and .pop add and remove items

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,5 +1,6 @@
 var Stack = function() {
   this.stackSize = 0;
+  this.storage = {};
 };
 
 Stack.prototype.size = function(){
@@ -7,11 +8,15 @@ Stack.prototype.size = function(){
 };
 
 Stack.prototype.push = function(value){
+  this.storage[this.stackSize] = value;
   this.stackSize++;
 };
 
 Stack.prototype.pop = function(){
   if(this.stackSize > 0){
     this.stackSize--;
+    var value = this.storage[this.stackSize];
+    delete this.storage[this.stackSize];
+    return value;
   }
 };
