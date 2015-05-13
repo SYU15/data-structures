675ef1106b57da6385ffb51499bb98d245ebf6bf
SYU88 <sarah.m.yu88@gmail.com>
2015-05-13 14:40:49 -0700

    Functional stack can now .push and .pop items

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -7,12 +7,16 @@ var Stack = function(){
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[size] = value;
     size++;
   };
 
   someInstance.pop = function(){
     if(size > 0){
+      var value = storage[size - 1];
+      delete storage[size - 1];
       size--;
+      return value;
     }
   };
 
