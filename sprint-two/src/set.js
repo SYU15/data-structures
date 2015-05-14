b598fc38f0347056712ee98ac9dc02d9c2eaccc1
SYU88 <sarah.m.yu88@gmail.com>
2015-05-14 16:31:42 -0700

    Set: .add adds values to set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,15 +1,19 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if(!_.contains(this._storage, item)){
+    this._storage[item] = true;
+  }
 };
 
 setPrototype.contains = function(item){
+  return _.contains(Object.keys(this._storage), item);
 };
 
 setPrototype.remove = function(item){
