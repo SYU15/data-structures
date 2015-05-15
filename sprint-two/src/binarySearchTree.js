8cae7093d53fddd99b904d3bd36154b4206b2cbf
SYU88 <sarah.m.yu88@gmail.com>
2015-05-14 20:54:36 -0700

    binarySearchTree: extend BinarySearchTree with methods .insert, .contains, .depthFirstLog

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,22 @@
 var BinarySearchTree = function(value){
+  var newBinarySearchTree = {};
+  _.extend(newBinarySearchTree, binarySearchTreeMethods);
+  return newBinarySearchTree;
 };
 
+var binarySearchTreeMethods = {};
 
+binarySearchTreeMethods.insert = function(value){
+
+};
+
+binarySearchTreeMethods.contains = function(target){
+
+};
+
+binarySearchTreeMethods.depthFirstLog = function(){
+
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
