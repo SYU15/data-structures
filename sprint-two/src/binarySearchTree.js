304cfec72d7afd42b09fde39bd56cfca0a67aed4
SYU88 <sarah.m.yu88@gmail.com>
2015-05-14 21:31:59 -0700

    binarySearchTree: create .compare function

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,9 @@
 var BinarySearchTree = function(value){
   var newBinarySearchTree = {};
+  newBinarySearchTree.value = value;
+  newBinarySearchTree.left = null;
+  newBinarySearchTree.right = null;
+
   _.extend(newBinarySearchTree, binarySearchTreeMethods);
   return newBinarySearchTree;
 };
@@ -7,7 +11,8 @@ var BinarySearchTree = function(value){
 var binarySearchTreeMethods = {};
 
 binarySearchTreeMethods.insert = function(value){
-
+  var newTree = BinarySearchTree(value);
+  var position = this.compare(value);
 };
 
 binarySearchTreeMethods.contains = function(target){
@@ -17,6 +22,23 @@ binarySearchTreeMethods.contains = function(target){
 binarySearchTreeMethods.depthFirstLog = function(){
 
 };
+
+binarySearchTreeMethods.compare = function(value){
+  if(value < this.value){
+    if(this.left === null){
+      return this;
+    } else {
+      return this.left.compare(value);
+    }
+  } else {
+    if(this.right === null){
+      return this;
+    } else{
+      return this.right.compare(value);
+    }
+  }
+};
+
 /*
  * Complexity: What is the time complexity of the above functions?
  */
