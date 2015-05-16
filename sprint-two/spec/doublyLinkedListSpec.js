9bde46e7894139c05b478dffc504e2ca09a16e7d
SYU88 <sarah.m.yu88@gmail.com>
2015-05-15 19:50:06 -0700

    doublyLinkedList: add tests for .addToHead and .removeTail

--- sprint-two/spec/doublyLinkedListSpec.js
+++ sprint-two/spec/doublyLinkedListSpec.js
@@ -16,6 +16,18 @@ describe('doublyLinkedList', function() {
     expect(doublyLinkedList.contains).to.be.a("function");
   });
 
+  it('should have methods named "addToHead" and "removeTail"', function(){
+    expect(doublyLinkedList.addToHead).to.be.a("function");
+    expect(doublyLinkedList.removeTail).to.be.a("function");
+  });
+
+  it('should designate a new head when addToHead is called', function(){
+    doublyLinkedList.addToHead(3);
+    expect(doublyLinkedList.head.value).to.equal(3);
+    doublyLinkedList.addToHead(4);
+    expect(doublyLinkedList.head.value).to.equal(4);
+  });
+
   it('should designate a new tail when new nodes are added', function(){
     doublyLinkedList.addToTail(4);
     expect(doublyLinkedList.tail.value).to.equal(4);
@@ -29,6 +41,14 @@ describe('doublyLinkedList', function() {
     expect(doublyLinkedList.head.value).to.equal(4);
     doublyLinkedList.removeHead();
     expect(doublyLinkedList.head.value).to.equal(5);
+  });
+
+  it('should remove tail from the list when removeTail is callled', function(){
+    doublyLinkedList.addToTail(5);
+    doublyLinkedList.addToTail(6);
+    expect(doublyLinkedList.tail.value).to.equal(6);
+    doublyLinkedList.removeTail();
+    expect(doublyLinkedList.tail.value).to.equal(5);
   });
 
   it("should return the value of the former head when removeHead is called", function(){
